f08cb08f4ba615677d56549345295a510b3e9b9b
Vitaly Medvedenko <vitalik.med2018@gmail.com>
2023-05-04 19:17:23 +0700

    Add playing field

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,33 @@
 #include <SFML/Graphics.hpp>
+#include <iostream>
 #include <time.h>
 
 using namespace sf;
+
+class Init
+{
+public:
+    Sprite cells[9]; // Всего 9 клеток
+    bool cellMode[9];
+
+    Init(Texture &image)
+    {
+        for (int i = 0; i < 9; i++)
+        {
+            cells[i].setTexture(image);
+            cellMode[i] = false;
+        }
+        for (int i = 0; i < 3; i++)
+            for (int j = 0; j < 3; j++)
+                cells[i * 3 + j].setPosition(200 * j, 200 * i);
+    }
+
+    void update(int &figure)
+    {
+        for (int i = 0; i < 9; i++)
+            cells[i].setTextureRect(IntRect(200 * (figure - 1), 0, 200, 200));
+    }
+};
 
 int main(int argc, char *argv[])
 {
@@ -16,6 +42,14 @@ int main(int argc, char *argv[])
         choice[i].setPosition(50 + 300 * i, 180);
     }
 
+    Texture bg;
+    bg.loadFromFile("img/background.png");
+    Sprite background(bg);
+
+    int result = 0; // 1 - крестик, 2 - нолик
+    Init player(figures);
+
+
     while (window.isOpen())
     {
         Vector2i mousePosition = Mouse::getPosition(window);
@@ -25,6 +59,26 @@ int main(int argc, char *argv[])
         {
             if (event.type == Event::Closed)
                 window.close();
+
+            if (Mouse::isButtonPressed(Mouse::Left))
+            {
+                if (result == 0)
+                {
+                    for (int i = 0; i < 2; i++)
+                    {
+                        if (choice[i].getGlobalBounds().contains(mousePosition.x, mousePosition.y))
+                            result = i + 1;
+                    }
+                }
+                else
+                {
+                    for (int i = 0; i < 9; i++)
+                    {
+                        if (player.cells[i].getGlobalBounds().contains(mousePosition.x, mousePosition.y))
+                            player.cellMode[i] = true;
+                    }
+                }
+            }
         }
 
         for (int i = 0; i < 2; i++)
@@ -35,9 +89,23 @@ int main(int argc, char *argv[])
                 choice[i].setTextureRect(IntRect(200 * i, 0, 200, 200));
         }
 
+        player.update(result);
         window.clear(Color::White);
-        for (int i = 0; i < 2; i++)
-            window.draw(choice[i]);
+
+        if (result == 0)
+        {
+            for (int i = 0; i < 2; i++)
+                window.draw(choice[i]);
+        }
+        else
+        {
+            for (int i = 0; i < 9; i++)
+                if (player.cellMode[i])
+                    window.draw(player.cells[i]);
+
+            window.draw(background);
+        }
+
         window.display();
     }
     
